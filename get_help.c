b285b8530402d4368d1489c8484854936c989434
Raymond <owizbluez@gmail.com>
2022-11-16 17:11:11 -0800

    update

--- get_help.c
+++ get_help.c
@@ -25,7 +25,7 @@ int get_help(data_shell *datash)
 	else if (_strcmp(datash->args[1], "alias") == 0)
 		aux_help_alias();
 
-	Else
+	else
 	write(STDERR_FILENO, datash->args[0],
 	      _strlen(datash->args[0]));
 		datash->status = 0;
